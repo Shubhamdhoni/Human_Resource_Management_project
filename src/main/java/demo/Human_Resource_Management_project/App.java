b27933626e466978cd2570a6c0f516fe6e2d37c0
Shubhamdhoni <shubhamdhoni030@gmail.com>
2024-11-17 11:34:45 +0530

    Initial commit

---
+++ src/main/java/demo/Human_Resource_Management_project/App.java
@@ -0,0 +1,10 @@
+package demo.Human_Resource_Management_project;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
